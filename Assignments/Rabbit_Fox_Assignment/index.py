ac2d5df83f557d00f509eed4beac7e58240d4455
Loopmootin <christoffera@outlook.dk>
2019-03-24 14:14:18 +0100

    python assignment fox and rabbit graph - fixed formular

--- Assignments/Rabbit_Fox_Assignment/index.py
+++ Assignments/Rabbit_Fox_Assignment/index.py
@@ -4,16 +4,8 @@ import math
 root = Tk()
 root.title("Population curve")
 
-c = Canvas(width=500, height=300, background='white')
-c.pack()
-
-axes = c.create_line(50, 150, 450, 150, fill='green')
-
 rabbit = 500.0
 fox = 10.0
-
-time = 0.0
-endTime = 400.0
 
 a = 0.1
 b = 0.00002
@@ -22,29 +14,38 @@ d = 0.01
 e = 0.00002
 f = 0.0001
 
-resultRabbit = rabbit * time * (1.0 + a - b * rabbit * time - c * fox * time)
-resultFox = fox * time * (1 - d + e * rabbit * time - f * fox * time)
-
 xyRabbit = []
 xyFox = []
 
-while time < endTime:
-    resultRabbit = rabbit * time * (1.0 + a - b * rabbit * time - c * fox * time)
-    resultFox = fox * time * (1 - d + e * rabbit * time - f * fox * time)
+for x in range(400):
+    resultRabbit = rabbit * (1.0 + a - b * rabbit - c * fox)
+    resultFox = fox * (1 - d + e * rabbit - f * fox)
 
-    print("Regn mig ud kanin", resultRabbit)
-    print("Regn mig ud røv", resultFox)
+    # print("Rabbit", int(resultRabbit))
+    # print("Fox", int(resultFox))
 
-    xyRabbit.append(50 + time)
-    xyRabbit.append(150 - math.sin(time/40) * 100)
+    rabbit = resultRabbit
+    fox = resultFox
 
-    xyFox.append(50 + time)
-    xyFox.append(150 - math.sin(time/40) * 100)
+    xyRabbit.append(50 + int(x))
+    xyRabbit.append(55 + int(resultRabbit))
 
-    time = time + 1
+    xyFox.append(50 + int(x))
+    xyFox.append(540 + int(resultFox))
+
+    # time = time + 1
 print()
 
-# rabbitCurve = c.create_line(xyRabbit, fill='blue')
-# foxCurve = c.create_line(xyFox, fill='red')
+print("Rabbit list", xyRabbit)
+print("Fox list", xyFox)
+
+c = Canvas(width=600, height=1000, background='white')
+c.pack()
+
+xaxes = c.create_line(50, 550, 500, 550, fill='green')
+# yaxes = c.create_line(50, 50, 50, 600, fill='green')
+
+rabbitCurve = c.create_line(xyRabbit, fill='blue')
+foxCurve = c.create_line(xyFox, fill='red')
 
 root.mainloop()
