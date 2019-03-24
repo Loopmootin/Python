91518d39a7db22e0c1bd2516ecd86a8081d87edf
Loopmootin <christoffera@outlook.dk>
2019-03-24 15:07:02 +0100

    adding average

--- Assignments/Rabbit_Fox_Assignment/index.py
+++ Assignments/Rabbit_Fox_Assignment/index.py
@@ -17,6 +17,9 @@ f = 0.0001
 xyRabbit = []
 xyFox = []
 
+steadyRabbit = []
+steadyFox = []
+
 for x in range(400):
     resultRabbit = rabbit * (1.0 + a - b * rabbit - c * fox)
     resultFox = fox * (1 - d + e * rabbit - f * fox)
@@ -28,22 +31,34 @@ for x in range(400):
     fox = resultFox
 
     xyRabbit.append(50 + int(x))
-    xyRabbit.append(55 + int(resultRabbit))
+    xyRabbit.append(-95 + int(resultRabbit))
+
+    steadyRabbit.append(int(resultRabbit))
 
     xyFox.append(50 + int(x))
-    xyFox.append(540 + int(resultFox))
+    xyFox.append(90 + int(resultFox))
 
-    # time = time + 1
+    steadyFox.append(int(resultFox))
+
 print()
 
-print("Rabbit list", xyRabbit)
-print("Fox list", xyFox)
+# print("Rabbit list", xyRabbit)
+# print("Fox list", xyFox)
 
-c = Canvas(width=600, height=1000, background='white')
+avgRabbit = sum(steadyRabbit)/len(steadyRabbit)
+avgFox = sum(steadyFox)/len(steadyFox)
+
+c = Canvas(width=600, height=800, background='white')
 c.pack()
 
-xaxes = c.create_line(50, 550, 500, 550, fill='green')
-# yaxes = c.create_line(50, 50, 50, 600, fill='green')
+c.create_text(150, 335, font=("Arial", 12), text="Blue graph is rabbit", fill='blue')
+c.create_text(150, 35, font=("Arial", 12), text="Red graph is fox", fill='red')
+
+c.create_text(150, 650, font=("Arial", 12), text="Average foxes : " + str(avgFox), fill='red')
+c.create_text(150, 700, font=("Arial", 12), text="Average rabbits : " + str(avgRabbit), fill='blue')
+
+axes1 = c.create_line(50, 400, 500, 400, fill='green')
+axes2 = c.create_line(50, 100, 500, 100, fill='green')
 
 rabbitCurve = c.create_line(xyRabbit, fill='blue')
 foxCurve = c.create_line(xyFox, fill='red')
