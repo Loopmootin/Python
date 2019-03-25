c2ef3cb45b10a2ceebb467e9ca474e80ffb61b04
Loopmootin <christoffera@outlook.dk>
2019-03-25 14:33:58 +0100

    multiplying fox results and setting steady value

--- Assignments/Rabbit_Fox_Assignment/index.py
+++ Assignments/Rabbit_Fox_Assignment/index.py
@@ -17,9 +17,6 @@ f = 0.0001
 xyRabbit = []
 xyFox = []
 
-steadyRabbit = []
-steadyFox = []
-
 for x in range(400):
     resultRabbit = rabbit * (1.0 + a - b * rabbit - c * fox)
     resultFox = fox * (1 - d + e * rabbit - f * fox)
@@ -30,35 +27,34 @@ for x in range(400):
     rabbit = resultRabbit
     fox = resultFox
 
-    xyRabbit.append(50 + int(x))
-    xyRabbit.append(-95 + int(resultRabbit))
+    xyRabbit.append(50 + x)
+    xyRabbit.append(-95 + resultRabbit)
 
-    steadyRabbit.append(int(resultRabbit))
-
-    xyFox.append(50 + int(x))
-    xyFox.append(90 + int(resultFox))
-
-    steadyFox.append(int(resultFox))
+    xyFox.append(50 + x)
+    xyFox.append(90 + resultFox * 35)
 
 print()
 
 print("Rabbit list", xyRabbit)
 print("Fox list", xyFox)
 
-avgRabbit = sum(steadyRabbit)/len(steadyRabbit)
-avgFox = sum(steadyFox)/len(steadyFox)
+steadyRabbit = str(int(xyRabbit[-1]))
+steadyFox = str(int(xyFox[-1] / 35))
+
+print(steadyRabbit)
+print(steadyFox)
+
 
 c = Canvas(width=600, height=800, background='white')
 c.pack()
 
-c.create_text(150, 335, font=("Arial", 12), text="Blue graph is rabbit", fill='blue')
-c.create_text(150, 35, font=("Arial", 12), text="Red graph is fox", fill='red')
+c.create_text(150, 280, font=("Arial", 12), text="Blue graph is rabbit", fill='blue')
+c.create_text(150, 300, font=("Arial", 12), text="Red graph is fox", fill='red')
 
-c.create_text(150, 650, font=("Arial", 12), text="Average foxes : " + str(avgFox), fill='red')
-c.create_text(150, 700, font=("Arial", 12), text="Average rabbits : " + str(avgRabbit), fill='blue')
+c.create_text(150, 35, font=("Arial", 12), text="Rabbit steady value : " + steadyRabbit, fill='blue')
+c.create_text(150, 55, font=("Arial", 12), text="Fox steady value : " + steadyFox, fill='red')
 
 axes1 = c.create_line(50, 400, 500, 400, fill='green')
-axes2 = c.create_line(50, 100, 500, 100, fill='green')
 
 rabbitCurve = c.create_line(xyRabbit, fill='blue')
 foxCurve = c.create_line(xyFox, fill='red')
