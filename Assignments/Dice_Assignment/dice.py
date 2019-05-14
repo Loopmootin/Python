8986dd267570aa4c3432a9777f3716b2e6597517
Loopmootin <christoffera@outlook.dk>
2019-05-14 15:27:09 +0200

    fixing functionality of game, setting up user interface and creating a die class in a seperate file

---
+++ Assignments/Dice_Assignment/dice.py
@@ -0,0 +1,25 @@
+# red_die = [0, 0, 4, 4, 8, 8]
+# green_die = [2, 2, 3, 3, 7, 7]
+# blue_die = [1, 1, 5, 5, 6, 6]
+
+
+class Die:
+    # constructor
+    def __init__(self, color, sides, scores):
+        self._color = color
+        self._sides = sides
+        self._scores = scores
+
+    # get
+    def getColor(self):
+        return self._color
+
+    def getSides(self):
+        return self._sides
+
+    def getScores(self):
+        return self._scores
+
+    def toString(self):
+        return self._color + " die has the sides " + str(self._sides)
+        + " and an empty score list " + str(self._scores)
--- Assignments/Dice_Assignment/game.py
+++ Assignments/Dice_Assignment/game.py
@@ -1,92 +1,144 @@
+# imports
 import random
+from dice import Die
 
-red_die = [0, 0, 4, 4, 8, 8]
-green_die = [2, 2, 3, 3, 7, 7]
-blue_die = [1, 1, 5, 5, 6, 6]
+# creating the dice from Die class
+red_die = Die("Red", [0, 0, 4, 4, 8, 8], [])
+green_die = Die("Green", [2, 2, 3, 3, 7, 7], [])
+blue_die = Die("Blue", [1, 1, 5, 5, 6, 6], [])
 
-red_die_list = []
-green_die_list = []
-blue_die_list = []
+# calling the winner
+the_winner = "Wait what, no winner??"
+
+# input from user
+numberofdice = int(input("Choose number of players(2 or 3): "))
+numberofrolls = int(input("Choose number of rolls: "))
 
 
+# if using 2 colors
 def roll_die_2():
     i = 0
     num = numberofrolls
+    # setting global to reuse same variable outside function
+    global first_player, second_player
 
-    if(choose_player1 == 1):
+    # setting first players color
+    if(choose_player1.lower() == "red"):
         first_player = red_die
-        first_player_list = red_die_list
-    elif(choose_player1 == 2):
+    elif(choose_player1.lower() == "green"):
         first_player = green_die
-        first_player_list = green_die_list
-    else:
+    elif(choose_player1.lower() == "blue"):
         first_player = blue_die
-        first_player_list = blue_die_list
-
-    if(choose_player2 == 1):
-        second_player = red_die
-        second_player_list = red_die_list
-    elif(choose_player2 == 2):
-        second_player = green_die
-        second_player_list = green_die_list
     else:
-        second_player = blue_die
-        second_player_list = blue_die_list
+        print("Something went wrong")
 
+    # setting second players color and checking if not the same as first
+    if(choose_player2.lower() == "red" and choose_player1.lower() != "red"):
+        second_player = red_die
+    elif(choose_player2.lower() == "green"
+            and choose_player1.lower() != "green"):
+        second_player = green_die
+    elif(choose_player2.lower() == "blue"
+            and choose_player1.lower() != "blue"):
+        second_player = blue_die
+    else:
+        print("You need to choose a different color than player 1!")
+
+    # amount of rolls is decided by user when program is running
     while i < num:
-        roll1 = random.choices(first_player)[0]
-        roll2 = random.choices(second_player)[0]
+        roll1 = random.choices(first_player.getSides())[0]
+        roll2 = random.choices(second_player.getSides())[0]
         if(roll1 > roll2):
-            first_player_list.append("+")
+            first_player.getScores().append("+")
         else:
-            second_player_list.append("+")
+            second_player.getScores().append("+")
         i = i + 1
 
 
+# if using all 3 colors
 def roll_die_3():
     i = 0
     num = numberofrolls
 
+    # amount of rolls is decided by user when program is running
     while i < num:
-        roll1 = random.choices(red_die)[0]
-        roll2 = random.choices(green_die)[0]
-        roll3 = random.choices(blue_die)[0]
+        roll1 = random.choices(red_die.getSides())[0]
+        roll2 = random.choices(green_die.getSides())[0]
+        roll3 = random.choices(blue_die.getSides())[0]
         if(roll1 > roll2 and roll1 > roll3):
-            red_die_list.append("+")
+            red_die.getScores().append("+")
         elif(roll2 > roll1 and roll2 > roll3):
-            green_die_list.append("+")
+            green_die.getScores().append("+")
         elif(roll3 > roll1 and roll3 > 2):
-            blue_die_list.append("+")
+            blue_die.getScores().append("+")
         i = i + 1
 
 
-numberofdice = int(input("Choose number of players(2 or 3): "))
-numberofrolls = int(input("Choose number of rolls: "))
-
+# chosing which function to run and what to print from the results
 if(numberofdice == 2):
-    first_player = int(input("Choose first player with number [1, 2, 3]: "))
-    sescond_player = int(input("Choose second player with number [1, 2, 3]: "))
+    choose_player1 = input("Choose which color die " +
+                           "you want [Red, Green, Blue]: ")
+    # calling the Die toString() 
+    if(choose_player1.lower() == "red"):
+        print(red_die.toString())
+    elif(choose_player1.lower() == "green"):
+        print(green_die.toString())
+    elif(choose_player1.lower() == "blue"):
+        print(blue_die.toString())
+    else:
+        print("You must chose one of the 3 colors!")
 
+    choose_player2 = input("Choose which color die " +
+                           "you want [Red, Green, Blue]: ")
+    # calling the Die toString()
+    if(choose_player2.lower() == "red"):
+        print(red_die.toString())
+    elif(choose_player2.lower() == "green"):
+        print(green_die.toString())
+    elif(choose_player2.lower() == "blue"):
+        print(blue_die.toString())
+    else:
+        print("You must chose one of the 3 colors!")
+
+    # calling function
     roll_die_2()
-    first_length = len(first_player_list)
-    second_length = len(second_player_list)
 
-    print(str(first_player) + " : " + str(first_length))
-    print(str(sescond_player) + " : " + str(second_length))
+    # checking and calling the winner
+    if(len(first_player.getScores()) > len(second_player.getScores())):
+        the_winner = first_player.getColor() + " is the winner!"
+    else:
+        the_winner = second_player.getColor() + " is the winner!"
+
+    print(str(first_player.getColor()) + " : " +
+          str(len(first_player.getScores())))
+    print(str(second_player.getColor()) + " : " +
+          str(len(second_player.getScores())))
+    print(the_winner)
 
 elif(numberofdice == 3):
-    choose_player1 = 1
-    choose_player2 = 2
-    choose_player3 = 3
-
+    # calling function
     roll_die_3()
-    red_length = len(red_die_list)
-    green_length = len(green_die_list)
-    blue_length = len(blue_die_list)
 
-    print(str(red_die) + " : " + str(red_length))
-    print(str(green_die) + " : " + str(green_length))
-    print(str(blue_die) + " : " + str(blue_length))
+    # checking and calling the winner
+    if(len(red_die.getScores()) > len(green_die.getScores()) and
+       len(red_die.getScores()) > len(blue_die.getScores())):
+        the_winner = red_die.getColor() + " is the winner!"
+    elif(len(green_die.getScores()) > len(red_die.getScores()) and
+         len(green_die.getScores()) > len(blue_die.getScores())):
+        the_winner = green_die.getColor() + " is the winner!"
+    elif(len(blue_die.getScores()) > len(red_die.getScores()) and
+         len(blue_die.getScores()) > len(green_die.getScores())):
+        the_winner = blue_die.getColor() + " is the winner!"
+    else:
+        the_winner = "Something went very wrong here!"
+
+    print(red_die.toString())
+    print(green_die.toString())
+    print(blue_die.toString())
+    print(str(red_die.getColor()) + " : " + str(len(red_die.getScores())))
+    print(str(green_die.getColor()) + " : " + str(len(green_die.getScores())))
+    print(str(blue_die.getColor()) + " : " + str(len(blue_die.getScores())))
+    print(the_winner)
 
 else:
-    print("Game doesn't work with " + numberofdice + " dice")
+    print("Game doesn't work with " + str(numberofdice) + " dice")
